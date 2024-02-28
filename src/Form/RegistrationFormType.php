88cdb77cd88819ffc9b116c1498aaa7f68f32efc
devJeremie <chabanaisjeremie@gmail.com>
2024-02-28 09:53:09 +0100

    >CrudRolesEnd

--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -23,7 +23,7 @@ class RegistrationFormType extends AbstractType
                 'mapped' => false,
                 'constraints' => [
                     new IsTrue([
-                        'message' => 'You should agree to our terms.',
+                        'message' => 'Accpeter les conditions générales est obligatoire !!',
                     ]),
                 ],
             ])
@@ -39,11 +39,11 @@ class RegistrationFormType extends AbstractType
                 'attr' => ['autocomplete' => 'new-password'],
                 'constraints' => [
                     new NotBlank([
-                        'message' => 'Please enter a password',
+                        'message' => 'Entrez un mot de passe !!',
                     ]),
                     new Length([
                         'min' => 6,
-                        'minMessage' => 'Your password should be at least {{ limit }} characters',
+                        'minMessage' => ' Votre mot de passe doit contenir{{ limit }} characters',
                         // max length allowed by Symfony for security reasons
                         'max' => 4096,
                     ]),
